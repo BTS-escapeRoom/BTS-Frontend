6709cb5b05b9c4b4df502563afd3c708f034c920
hanjiwon1 <wldnjs20725@gmail.com>
2025-06-13 16:04:17 +0900

    fix: 버튼 렌더링 안되는 이슈 수정

--- src/app/(auth)/login/SocialLoginButtonList.tsx
+++ src/app/(auth)/login/SocialLoginButtonList.tsx
@@ -1,32 +1,44 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { SocialLoginButton } from './SocialLoginButton'
 import IconKakao from '@/components/icons/IconKakao'
 import IconNaver from '@/components/icons/IconNaver'
 import IconApple from '@/components/icons/IconApple'
 
 export const SocialLoginButtonList = () => {
+  const [origin, setOrigin] = useState('')
+
+  useEffect(() => {
+    setOrigin(window.location.origin)
+  }, [])
+
   const handleLogin = (provider: 'kakao' | 'naver' | 'apple') => {
-    window.location.href = `https://apis.bangtal-boys.com/oauth2/authorization/${provider}?return-url=${window.location.origin}/oauth/callback`
+    if (!origin) return
+    window.location.href = `https://apis.bangtal-boys.com/oauth2/authorization/${provider}?return-url=${origin}/oauth/callback`
   }
 
   return (
     <div className="flex w-[280px] flex-col gap-[8px]">
-      <SocialLoginButton
-        provider="kakao"
-        onClick={() => handleLogin('kakao')}
-        icon={<IconKakao />}
-      />
-      <SocialLoginButton
-        provider="naver"
-        onClick={() => handleLogin('naver')}
-        icon={<IconNaver />}
-      />
-      <SocialLoginButton
-        provider="apple"
-        onClick={() => handleLogin('apple')}
-        icon={<IconApple />}
-      />
+      {origin && (
+        <>
+          <SocialLoginButton
+            provider="kakao"
+            onClick={() => handleLogin('kakao')}
+            icon={<IconKakao />}
+          />
+          <SocialLoginButton
+            provider="naver"
+            onClick={() => handleLogin('naver')}
+            icon={<IconNaver />}
+          />
+          <SocialLoginButton
+            provider="apple"
+            onClick={() => handleLogin('apple')}
+            icon={<IconApple />}
+          />
+        </>
+      )}
     </div>
   )
 }
