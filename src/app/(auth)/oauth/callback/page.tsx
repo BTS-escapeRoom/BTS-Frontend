b38a6c9f3d56718b0c15d2b09ca9896e0ba4b215
hanjiwon1 <wldnjs20725@gmail.com>
2025-06-17 18:21:41 +0900

    fix: 로그인 완료 후 회원가입 페이지로 이동

--- src/app/(auth)/oauth/callback/page.tsx
+++ src/app/(auth)/oauth/callback/page.tsx
@@ -20,7 +20,8 @@ function CallbackContent() {
         router.replace('/')
         break
       case 'signup':
-        router.replace('/nickname')
+      case 'emptyNickname':
+        router.replace('/signup')
         break
       default:
         alert('일시적인 오류로 로그인에 실패했습니다. 잠시 후 다시 시도해주세요.')
