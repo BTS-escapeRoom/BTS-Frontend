6f74bfd3d1adf5e4b9edb5789dff9a20b69e883f
hanjiwon1 <wldnjs20725@gmail.com>
2025-08-26 22:01:19 +0900

    feat: not fount 페이지 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,16 +5,18 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className="fixed inset-0 z-[100] flex items-center justify-center">
-      <Image
-        src="/images/full-page-keyhole.png"
-        alt="404 background"
-        fill
-        className="object-cover"
-      />
-      <div className="relative z-10 text-center text-gray-800">
-        <h1 className="text-[64px] font-bold leading-none">404</h1>
-        <p className="mt-2 text-[24px] font-semibold">여긴 막다른 길이에요 😅</p>
-        <SButton as={Link} href="/theme" className="mt-[16px] bg-gray-800">
+      <div className="absolute mx-auto min-h-screen w-full max-w-[600px]">
+        <Image
+          src="/images/full-page-keyhole.png"
+          alt="404 background"
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div className="relative z-10 text-center text-gray07">
+        <h1 className="text-[64px] font-bold leading-none text-gray05">404</h1>
+        <p className="mt-2 text-[24px] font-semibold">여긴 막다른 길이에요 🚪</p>
+        <SButton as={Link} href="/theme" className="mt-[16px] bg-gray07">
           홈으로 탈출하기
         </SButton>
       </div>
