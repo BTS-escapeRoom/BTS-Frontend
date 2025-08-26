1733faaa4fa705a23ceeb38241092217ec8866d4
hanjiwon1 <wldnjs20725@gmail.com>
2025-08-26 22:01:00 +0900

    스토리북 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { FlatCompat } from '@eslint/eslintrc'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
@@ -8,20 +11,17 @@ const __dirname = dirname(__filename)
 const compat = new FlatCompat({ baseDirectory: __dirname })
 
 /** @type {import("eslint").Linter.FlatConfig[]} */
-const config = [
-  ...compat.extends([
-    'next',
-    'next/core-web-vitals',
-    'prettier',
-  ]),
-  {
-    plugins: {
-      prettier: require('eslint-plugin-prettier'),
-    },
-    rules: {
-      'prettier/prettier': 'error',
-    },
+const config = [...compat.extends([
+  'next',
+  'next/core-web-vitals',
+  'prettier',
+]), {
+  plugins: {
+    prettier: require('eslint-plugin-prettier'),
   },
-]
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}, ...storybook.configs["flat/recommended"], ...storybook.configs["flat/recommended"]]
 
 export default config
