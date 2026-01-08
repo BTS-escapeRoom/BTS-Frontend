20ad4ea4760a9035a03a85d900879dfa9ab63054
hanjiwon1 <wldnjs20725@gmail.com>
2026-01-08 22:40:31 +0900

    fix: 테마 연결 안되는 이슈 수정

--- src/app/(community)/board/recruit/write/components/RecruitThemeField.tsx
+++ src/app/(community)/board/recruit/write/components/RecruitThemeField.tsx
@@ -22,11 +22,20 @@ export default function RecruitThemeField({ theme, onClick }: RecruitThemeFieldP
     <div className="flex flex-col gap-[4px]">
       <div className="text-[14px] font-bold text-gray06">테마 정보</div>
       {hasTheme ? (
-        <ThemeInfoItem theme={theme as Theme} isSelected={false} onClick={onClick} infoText="변경" />
+        <ThemeInfoItem
+          theme={theme as Theme}
+          isSelected={false}
+          onClick={onClick}
+          infoText="변경"
+        />
       ) : (
         <button
           type="button"
-          onClick={onClick}
+          onClick={(e) => {
+            e.preventDefault()
+            e.stopPropagation()
+            onClick()
+          }}
           className="flex w-full items-center justify-between rounded-[4px] bg-[#fafafa] px-[16px] py-[16px]"
         >
           <div className="flex flex-1 items-start justify-center">
